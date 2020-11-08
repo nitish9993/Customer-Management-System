afddb82e99d669ab17b901011adf41bdaefb77de
Nitish <nitishsingh9993@gmail.com>
2020-11-09 00:49:50 +0530

    first Commit

---
+++ src/main/java/com/customermanagement/cms/exception/CustomerNotFoundException.java
@@ -0,0 +1,8 @@
+package com.customermanagement.cms.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
